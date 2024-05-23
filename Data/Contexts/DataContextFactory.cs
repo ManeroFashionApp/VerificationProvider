1ca49ea232c3fdb722395e73ee64fd3b06f1bdc5
CiaOlsson <146974056+CiaOlsson@users.noreply.github.com>
2024-05-23 10:19:29 +0200

    GenerateAndSendVerificationCode done

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Contexts;
+
+public class DataContextFactory: IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        var connectionString = "DatabaseConnection";
+        optionsBuilder.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+        });
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
