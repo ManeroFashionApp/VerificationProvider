9e80f699a05aace38fccc50dafa43b9337820b1e
CiaOlsson <146974056+CiaOlsson@users.noreply.github.com>
2024-05-24 11:50:32 +0200

    Validate code function done and tested locally.

---
+++ VerificationProvider/Services/IValidateCodeService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services
+{
+    public interface IValidateCodeService
+    {
+        Task<ValidateCodeRequest> UnpackValidateRequestAsync(HttpRequest req);
+        Task<bool> ValidateCodeAsync(ValidateCodeRequest validateRequest);
+    }
+}
